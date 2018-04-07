b09763fe081467ae2a63e867c2af0430a7213c2e
lcy <sisbeau@126.com>
2018-04-07 16:53:17 +0800

    add example.js, showing the usage of bootact components.

---
+++ example.js
@@ -0,0 +1,78 @@
+import React, { Component } from 'react';
+
+import {Button, ListGroup as List, Badge, Label, Alert} from './bootact';
+import {Progress, Icon, Row, Col} from './bootact';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      id: 3
+    };
+  }
+  render() {
+    const dataList = [
+      {
+        lang: "Python",
+        count: 3
+      },
+      {
+        lang: "C",
+        count: "105"
+      },
+      {
+        lang: "Java",
+        count: "116"
+      }
+    ];
+    return (
+      <div className="App">
+        <Button type="success" data-id={this.state.id} onClick={this.handleClick.bind(this)}>
+        确认<Icon type="ok"/>
+        </Button>
+        <List
+          dataSource={dataList}
+          renderItem={(item,k) => (<List.Item key={k} data-key={k} type={item.count>100 ? 'success' : ''}>{item.lang}<Badge text={item.count}/></List.Item>)}
+        />
+        <Label type="info">Hello</Label>
+        <Alert type="info" withClose>Well done!</Alert>
+        <Progress
+          type="info" 
+          striped
+          active
+          percent={35}
+          template={"%d completed"}
+        />
+        <Row>
+          <Col span={4}>col-4</Col>
+          <Col span={4}>col-4</Col>
+          <Col span={4}>col-4</Col>
+        </Row>
+        <Row>
+          <Col span={4}>col-4</Col>
+          <Col span={4} offset={2}>col-4</Col>
+        </Row>
+        <Row>
+          <Col span={4}>col-4</Col>
+          <Col span={4} push={2}>col-4</Col>
+        </Row>
+        <Row>
+          <Col span={4}>col-4</Col>
+          <Col span={4} pull={2}>col-4</Col>
+        </Row>
+        <Row>
+          <Col sm={4}>col-4</Col>
+          <Col sm={{span:4, offset: 2}}>col-4</Col>
+        </Row>
+      </div>
+    );
+  }
+  handleClick() {
+    const state = this.state;
+    this.setState({
+      id: state.id+1
+    });
+  }
+}
+
+export default App;
